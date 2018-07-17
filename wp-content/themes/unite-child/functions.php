e231d386b58302e0c96885d335507314ea1b2d32
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2018-07-17 20:00:34 +0530

    Setup child theme : unite-child

---
+++ wp-content/themes/unite-child/functions.php
@@ -0,0 +1,14 @@
+<?php
+function unite_child_theme_enqueue_styles() {
+
+    $parent_style = 'unite-style';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'unite_child_theme_enqueue_styles' );
+?>
